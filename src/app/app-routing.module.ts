1b7b92b39511ce468744a7934afbf145b7b96d4b
gchan26 <guilhermechan30@gmail.com>
2022-10-17 15:00:20 -0300

    Finished project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// Guard
+import { AuthGuard } from './core/guard/auth.guard';
+
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./core/components/auth/auth.module').then(m => m.AuthModule)
   }, {
     path: 'admin',
+    canActivateChild: [AuthGuard],
     loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule)
   }
 ];

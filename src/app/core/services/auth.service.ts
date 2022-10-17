33f5f0e87024a16a2614cd6958d6f4f61ee083c6
gchan26 <guilhermechan30@gmail.com>
2022-10-17 14:41:25 -0300

    Fixed admin page layout

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,25 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { catchError, map, Observable, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   private url: string = 'http://localhost:3000';
 
-  constructor( private http: HttpClient ) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
-  public sign(payLoad: {email: string, password: string}): Observable<any>{
-    return this.http.post(`${this.url}/sign`, payLoad).pipe(
+  public sign(payLoad: { email: string; password: string }): Observable<any> {
+    return this.http.post<{ token: string }>(`${this.url}/sign`, payLoad).pipe(
       map((res) => {
-        return console.log(res);
+        localStorage.removeItem('access_token');
+        localStorage.setItem('access_token', res.token);
+        return this.router.navigate(['admin']);
       }),
-      catchError((e)=> {
-        if(e.error.message) return throwError(() => e.error.message);
+      catchError((e) => {
+        if (e.error.message) return throwError(() => e.error.message);
 
-        return throwError(() => "No momento não estamos conseguindo validar estes dados, tente novamente mais tarde.");
+        return throwError(
+          () =>
+            'No momento não estamos conseguindo validar estes dados, tente novamente mais tarde.'
+        );
       })
     );
   }
